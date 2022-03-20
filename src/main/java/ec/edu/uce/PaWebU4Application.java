8f51fdeb5487d69b01adccd837ad981de16f68ce
driven <aldazpaulo@gmail.com>
2022-03-19 19:15:03 -0500

    procesamiento sincrono

--- src/main/java/ec/edu/uce/PaWebU4Application.java
+++ src/main/java/ec/edu/uce/PaWebU4Application.java
@@ -1,13 +1,42 @@
 package ec.edu.uce;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PaWebU4Application {
+import ec.edu.uce.service.IProcesamientoFacultadService;
 
+
+@SpringBootApplication
+public class PaWebU4Application implements CommandLineRunner{
+	
+	private static final Logger LOG = LoggerFactory.getLogger(PaWebU4Application.class);
+	
+	@Autowired
+	private IProcesamientoFacultadService facu;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PaWebU4Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		long inicio = System.currentTimeMillis();
+		
+		Integer valor1 =  this.facu.calcularDeuda("1234567", 4);
+		Integer valor2 =  this.facu.calcularDeuda("12367", 4);
+		Integer valor3 = this.facu.calcularDeuda("1772567", 4);
+		Integer valor4 = this.facu.calcularDeuda("18784567", 4);
+		
+		LOG.info("TOTAL: " + (valor1 +valor2 + valor3+ valor4));
+		
+		long tFinal = System.currentTimeMillis();
+		long tTransc = (tFinal - inicio)/ 100;
+		LOG.info(tTransc + "seg");
 	}
 
 }
--- src/main/java/ec/edu/uce/paralelo/hilos/paralelo/Main.java
+++ src/main/java/ec/edu/uce/paralelo/hilos/paralelo/Main.java
@@ -7,24 +7,26 @@ public class Main {
 		
 		long tiempoInicial = System.currentTimeMillis();
 		
-		CajeroHilo cajero1 = new CajeroHilo("Edison", Arrays.asList(1,2,3,4,5));
-		CajeroHilo cajero2 = new CajeroHilo("Carlos", Arrays.asList(1,2,3,4));
-		CajeroHilo cajero3 = new CajeroHilo("Carla", Arrays.asList(2,3,3,4,5));
+		CajeroHilo cajero1 = new CajeroHilo("Edison", Arrays.asList(1,2,3,4));
+		CajeroHilo cajero2 = new CajeroHilo("Carlos", Arrays.asList(1,2,3));
+		CajeroHilo cajero3 = new CajeroHilo("Carla", Arrays.asList(2,3,3,4));
 		
+		System.out.println("Hilo principal: " + Thread.currentThread().getName());
 		GestorAtencionHilo gestor1 = new GestorAtencionHilo(cajero1, tiempoInicial);
 		GestorAtencionHilo gestor2 = new GestorAtencionHilo(cajero2, tiempoInicial);
 		GestorAtencionHilo gestor3 = new GestorAtencionHilo(cajero3, tiempoInicial);
-		
+		System.out.println("Hilo principal 2: " + Thread.currentThread().getName());
 		gestor1.start(); //procesar
 		gestor2.start();
 		gestor3.start();
-
+		System.out.println("Hilo principal 2: " + Thread.currentThread().getName());
 		
 		long tiempoFinal = System.currentTimeMillis();
 		
 		long tiempoTranscurrido = (tiempoFinal - tiempoInicial)/1000;
 		
 		System.out.println(tiempoTranscurrido + "seg");
+		System.out.println("Termino");
 	}
 
 }
