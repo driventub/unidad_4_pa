69dbb5f7bf63fce10e38e306a9abcd0254897db5
driven <aldazpaulo@gmail.com>
2022-03-19 20:21:27 -0500

    asincrona y async

--- src/main/java/ec/edu/uce/PaWebU4Application.java
+++ src/main/java/ec/edu/uce/PaWebU4Application.java
@@ -1,16 +1,21 @@
 package ec.edu.uce;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import ec.edu.uce.service.IProcesamientoFacultadService;
 
 
 @SpringBootApplication
+@EnableAsync
+
 public class PaWebU4Application implements CommandLineRunner{
 	
 	private static final Logger LOG = LoggerFactory.getLogger(PaWebU4Application.class);
@@ -27,15 +32,27 @@ public class PaWebU4Application implements CommandLineRunner{
 		
 		long inicio = System.currentTimeMillis();
 		
-		Integer valor1 =  this.facu.calcularDeuda("1234567", 4);
-		Integer valor2 =  this.facu.calcularDeuda("12367", 4);
-		Integer valor3 = this.facu.calcularDeuda("1772567", 4);
-		Integer valor4 = this.facu.calcularDeuda("18784567", 4);
+//		Procesamiento asincrono, pero sin manejo de respuesta por cada hilo
+//		Integer valor1 =  this.facu.calcularDeuda("1234567", 4);
+//		Integer valor2 =  this.facu.calcularDeuda("12367", 4);
+//		Integer valor3 = this.facu.calcularDeuda("1772567", 4);
+//		Integer valor4 = this.facu.calcularDeuda("18784567", 4);
 		
-		LOG.info("TOTAL: " + (valor1 +valor2 + valor3+ valor4));
+//		Procesamiento asincrono, pero sin manejo de respuesta por cada hilo
+		
+		CompletableFuture<Integer> valor1 =  this.facu.calcularDeudaFuture("1234567", 4);
+		CompletableFuture<Integer> valor2 =  this.facu.calcularDeudaFuture("12367", 4);
+		CompletableFuture<Integer> valor3 = this.facu.calcularDeudaFuture("1772567", 4);
+		CompletableFuture<Integer> valor4 = this.facu.calcularDeudaFuture("18784567", 4);
+		
+//		Sentencia que espera que terminen todos los hilos
+		CompletableFuture.allOf(valor1,valor2,valor3,valor4).join();
+		
+		LOG.info("TOTAL: " + (valor1.get() +valor2.get() + valor3.get()+ valor4.get()));
 		
 		long tFinal = System.currentTimeMillis();
 		long tTransc = (tFinal - inicio)/ 100;
+		
 		LOG.info(tTransc + "seg");
 	}
 
--- src/main/java/ec/edu/uce/service/ProcesamientoFacultadServiceImpl.java
+++ src/main/java/ec/edu/uce/service/ProcesamientoFacultadServiceImpl.java
@@ -1,32 +1,50 @@
 package ec.edu.uce.service;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
-public class ProcesamientoFacultadServiceImpl implements IProcesamientoFacultadService{
-	
+public class ProcesamientoFacultadServiceImpl implements IProcesamientoFacultadService {
+
 	private static final Logger LOG = LoggerFactory.getLogger(ProcesamientoFacultadServiceImpl.class);
-	
+
 	@Override
+	@Async
 	public Integer calcularDeuda(String cedula, Integer valor) {
-		
-		
-		
+
 		LOG.info("Procesando: " + cedula);
 		try {
-			TimeUnit.SECONDS.sleep(10);
+			TimeUnit.SECONDS.sleep(1);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return null;
-		
+
+		LOG.info("Finalizo: " + cedula);
+		return valor;
+
 	}
 	
+	@Override
+	@Async
+	public CompletableFuture<Integer> calcularDeudaFuture(String cedula, Integer valor) {
+
+		LOG.info("Procesando: " + cedula);
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		LOG.info("Finalizo: " + cedula);
+		return CompletableFuture.completedFuture(valor);
+
+	}
+
 }
