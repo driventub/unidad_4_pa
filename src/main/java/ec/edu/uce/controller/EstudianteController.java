fc436a8f16d9a678fab72c0bba6101005deb0c7b
driven <aldazpaulo@gmail.com>
2022-03-22 20:12:41 -0500

    ingresar con thymeleaf

--- src/main/java/ec/edu/uce/controller/EstudianteController.java
+++ src/main/java/ec/edu/uce/controller/EstudianteController.java
@@ -1,10 +1,14 @@
 package ec.edu.uce.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import ec.edu.uce.repository.modelo.Estudiante;
 import ec.edu.uce.service.IEstudianteService;
@@ -17,7 +21,9 @@ public class EstudianteController {
 	@Autowired
 	private IEstudianteService estuService;
 	
-	@RequestMapping("/buscar/{idEstudiante}")
+//	@RequestMapping("/buscar/{idEstudiante}")
+	@GetMapping("/buscar/{idEstudiante}")
+//	@RequestMapping(path = "/buscar/{idEstudiante}", method = RequestMethod.GET)
 	public String obtenerUsuario(@PathVariable("idEstudiante") Integer idEstudiante, Model modelo ) {
 		
 		Estudiante estu =  this.estuService.buscar(idEstudiante);
@@ -28,6 +34,25 @@ public class EstudianteController {
 		
 		modelo.addAttribute("estu" , estu);
 		return "estudiante";
+	}
+	
+	
+	@GetMapping("/buscar/todos")
+	public String buscarTodos(Model modelo) {
+		List<Estudiante> listaEstudiantes = this.estuService.buscarTodos();
+		modelo.addAttribute("estudiantes", listaEstudiantes );
+		return "lista";
+	}
+	
+//	Se necesitan dos paginas
+//	public String insertarEstudiante() {
+//		
+//	}
+	
+	@GetMapping("/estudianteNuevo")
+	public String obtenerDato() {
+		return "estudiante_nuevo";
+				
 	}
 	
 }
